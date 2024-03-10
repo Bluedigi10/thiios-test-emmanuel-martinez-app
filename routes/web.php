746eec03d5dc4a8c6e4049871bbd303f9dbed68c
Emmanuel Vidal <edmvp98@gmail.com>
2024-03-10 09:57:20 -0600

    a little functionality test

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use Inertia\Inertia;
 
 /*
